1c5352eabcec36dffa0e3fde5c5dd7ceb633e98e
mzaini30 <muhzaini30@gmail.com>
2024-06-26 15:13:58 +0800

    pakai yang versi laravel

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Mengenali .blade.php sebagai jenis file PHP
-vim.cmd("autocmd BufNewFile,BufRead *.blade.php set ft=php")
-
 vim.o.wrap = true
